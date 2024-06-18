726885ade61d9a563f700b5bc61ab957cb77d821
Lauri Sarap <lauri.aardla@gmail.com>
2024-06-18 11:47:21 +0300

    The zip stuff updates

--- app/parsers/dxf_parser.py
+++ app/parsers/dxf_parser.py
@@ -1,8 +1,24 @@
 from app.parsers.dxf_parsing_main import initialize
+import zipfile
+import os
 
 
 def parse_dxf(file_path):
-    page = initialize(file_path)  # Default values: visualize=False, save=False, analyze=True, log_times=False
-    return page
+    if file_path.endswith('.dxf'):
+        page = initialize(file_path)
+        return {"pages": [page]}
+    
+    elif file_path.endswith('.zip'):
+        pages = []
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            temp_dir = os.path.join(os.path.dirname(file_path), 'temp')
+            zip_ref.extractall(temp_dir)
+            for file in zip_ref.namelist():
+                if file.endswith('.dxf'):
+                    dxf_path = os.path.join(temp_dir, file)
+                    page = initialize(dxf_path)
+                    pages.append(page)
+                    os.remove(dxf_path)
+            os.rmdir(temp_dir)
+        return {"pages": pages}
 
-# parse_dxf("E:\\dev\\CADRAK2\\CADRAKEngine\\app\\parsers\\LauriToru.dxf")
--- app/routes.py
+++ app/routes.py
@@ -23,14 +23,15 @@ def parse_file():
         logger.error("No selected file")
         return jsonify({"error": "No selected file"}), 400
 
-    if not file.filename.endswith('.dxf'):
-        logger.error("Invalid file type. Only .dxf files are allowed")
-        return jsonify({"error": "Invalid file type. Only .dxf files are allowed"}), 400
+    if not (file.filename.endswith('.dxf') or file.filename.endswith('.zip')):
+        logger.error("Invalid file type. Only .dxf or .zip files are allowed")
+        return jsonify({"error": "Invalid file type. Only .dxf or .zip files are allowed"}), 400
 
     file_path = os.path.join(TEMP_UPLOAD_FOLDER, file.filename)
     file.save(file_path)
 
     parsed_data = parse_dxf(file_path)
+    logger.info(f"Parsed data: {parsed_data}")
     os.remove(file_path)
 
     return jsonify({"message": f"File {file.filename} parsed successfully", "parsed_data": parsed_data}), 200
